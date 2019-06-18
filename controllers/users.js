ecb16b7b7ad09941e892c2a54bf8060305de00e0
Matt Viteri <mxviteri@gmail.com>
2019-06-17 22:07:00 -0500

    init

---
+++ controllers/users.js
@@ -0,0 +1,67 @@
+const mysql = require('mysql')
+const pool = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
+
+const getAllUsers = (req, res) => {
+  // SELECT ALL USERS
+  pool.query("SELECT * FROM users", (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+}
+
+const getUserById = (req, res) => {
+  // SELECT USERS WHERE ID = <REQ PARAMS ID>
+  let sql = "QUERY GOES HERE"
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, [])
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+}
+
+const createUser = (req, res) => {
+  // INSERT INTO USERS FIRST AND LAST NAME 
+  let sql = "QUERY GOES HERE"
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, [])
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ newId: results.insertId });
+  })
+}
+
+const updateUserById = (req, res) => {
+  // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
+  let sql = ""
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, [])
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.status(204).json();
+  })
+}
+
+const deleteUserByFirstName = (req, res) => {
+  // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
+  let sql = ""
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, [])
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
+  })
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUserById,
+  deleteUserByFirstName
+}
---
+++ sql/connection.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql')
+
+class Connection {
+  constructor() {
+    if (!this.pool) {
+      console.log('creating connection...')
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'admin'
+      })
+
+      return this.pool
+    }
+
+    return this.pool
+  }
+}
+
+const instance = new Connection()
+
+module.exports = instance;
